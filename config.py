f08a709a4f68e6d9769feaee85974c8fd503a255
QuinnMcHenry <quinn.mchenry@student.chaminade.edu>
2025-01-23 19:23:08 -1000

    a

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ DB_PASSWORD = "Haloninja11"
 DB_HOST = "nobsv2"
 DB_NAME = "facilities"
 
-DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST:3306}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(bind=engine)
